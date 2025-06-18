1cf73cc337b8ecb5dbc08834def31d66131bd996
Damodar Edara <edaradamu@gmail.com>
2025-06-18 22:48:44 +0530

    Add swagger ui express to load the openapi yaml file in local

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,23 @@ import userRoutes from "./routes/users";
 import postRoutes from "./routes/posts";
 import authRoutes from "./routes/auth";
 import { errorHandler } from "./middleware/errorHandler";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Load YAML
+const swaggerDocument = YAML.load(
+  path.join(__dirname, "../docs/contract/openapi.yml")
+);
+
+// Setup Swagger route
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.DB_CONNECTION!;
